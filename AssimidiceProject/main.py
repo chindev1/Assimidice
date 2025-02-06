d71b2d37694282a6dedc80a61e2a774393c27d0f
Fttr <nfatorino@gmail.com>
2025-02-06 20:32:01 -0300

    Teste com função unica para os dados

--- AssimidiceProject/main.py
+++ AssimidiceProject/main.py
@@ -10,37 +10,25 @@ class Client(discord.Client):
 
         diceArray = [None] * int(first_letter)
         diceArray[1] = 0
-
         #print(f"Message from {message.author}: {message.content}")
         if message.author == self.user:
             return
-
-        if message.content[1].startswith("d") and message.content[2].startswith("6"):
-            dices = 0
-            i = 0
-            while dices < int(first_letter):
-                dice6 = randint(1, 6)
-                diceArray[i] = dice6
-                dices += 1
-                i += 1
-            formatted_list = ", ".join(map(str, diceArray))
-            await message.channel.send("```" + first_letter + f"d6 -> [" + formatted_list + "]```")
-
-
-        if message.content[1].startswith("d") and message.content[2].startswith("2") and message.content[3].startswith("0"):
-            dices = 0
-            i = 0
-            while dices < int(first_letter):
-                dice6 = randint(1, 20)
-                diceArray[i] = dice6
-                dices += 1
-                i += 1
-            formatted_list = ", ".join(map(str, diceArray))
-            await message.channel.send("```" + first_letter + f"d20 -> [" + formatted_list + "]```")
-
+        def dados():
+            m1 = message.content[1]
+            m2 = message.content[2]
+            if m1.startswith("d"):
+                dices = 0
+                i = 0
+                while dices < int(first_letter):
+                    dice6 = randint(1, m2)
+                    diceArray[i] = dice6
+                    dices += 1
+                    i += 1
+                formatted_list = ", ".join(map(str, diceArray))
+                message.channel.send("```" + first_letter + f"d" + m2 + " -> [" + formatted_list + "]```")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents = intents)
-client.run('')
+client.run('MTMyNzA0NzIwODkzMTE2ODMxNw.GaHBYw.TQB4X1z8slNUJzw13kMpjIK945NnFr2bQgqLnI')
