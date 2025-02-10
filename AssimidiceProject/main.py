1c5a2d650c901944abeb63d4eff925252fa514ef
Fttr <nfatorino@gmail.com>
2025-02-10 19:11:34 -0300

    Função para rodar dados de todas as faces; Enviar imagens de assimilação nos dados.

--- AssimidiceProject/main.py
+++ AssimidiceProject/main.py
@@ -96,4 +96,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('MTMyNzA0NzIwODkzMTE2ODMxNw.GaHBYw.TQB4X1z8slNUJzw13kMpjIK945NnFr2bQgqLnI')
+client.run('INSERT DISCORD TOKEN')
